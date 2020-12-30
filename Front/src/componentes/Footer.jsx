3bced642f2d86d909befe24ea25f96f0b1f4437f
Carlos M <carlosilleda@gmail.com>
2020-12-30 08:47:01 +0100

    Non relevant

--- Front/src/componentes/Footer.jsx
+++ Front/src/componentes/Footer.jsx
@@ -3,7 +3,7 @@ import Translate from './Translate'
 
 const Footer = (...props) => (
     <footer className="fixed-bottom footerTik mx-auto container">
-        <p className="float-right"><a className="footerLink" href="#"><Translate string={"Volver_arriba"}/></a></p>
+        <p className="float-right"><a className="footerLink" href="www.as.as"><Translate string={"Volver_arriba"}/></a></p>
         <p>&copy; TikTak, Inc. &middot; <a className="footerLink" href="https://github.com/"><Translate string={"Privacidad"}/></a> &middot; <a className="footerLink" href="https://github.com/"><Translate string={"Terminos"}/></a></p>
     </footer>
 )
